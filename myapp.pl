68d04a0c328de367e255e90f33cee06b45f77a43
Jaemin Choi <skyloader@gmail.com>
2012-08-26 00:16:27 +0900

    Show all article paths of specific year

--- myapp.pl
+++ myapp.pl
@@ -1,10 +1,30 @@
 #!/usr/bin/env perl
+
 use Mojolicious::Lite;
+
+use Skyloader::Blog::Article;
+
+my $sba = Skyloader::Blog::Article->new(
+    directory => '/Users/jaeminchoi/workspace/blog',
+);
 
 # Documentation browser under "/perldoc"
 plugin 'PODRenderer';
 
 get '/' => 'index';
+
+get '/article/:year' => sub {
+    my $self = shift;
+
+    my $year = $self->param('year');
+
+    my $articles = $sba->year($year);
+
+    $self->render(
+        'article',
+        articles => $articles,
+    );
+};
 
 app->start;
 __DATA__
@@ -13,6 +33,17 @@ __DATA__
 % layout 'default';
 % title 'Welcome';
 Skyloader-Blog
+
+
+@@ article.html.ep
+% layout 'default';
+% title 'Welcome';
+<ul>
+  % for my $article (@$articles) {
+    <li> <%= $article %> </li>
+  % }
+</ul>
+
 
 @@ layouts/default.html.ep
 <!DOCTYPE html>
